5d81fba9e2b614b10b1860ebe7903ba4fff8e2a5
jeronartest <jeronsmail@gmail.com>
2018-09-26 13:04:45 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Container } from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
